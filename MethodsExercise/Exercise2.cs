ab2f8b947492fd88d3e0656247e8a6dc43a8e5e2
Graham Spencer <gspencer447@yahoo.com>
2023-08-25 14:29:30 -0500

    completed both exercises and completed the challenge portion of exercise 2

---
+++ MethodsExercise/Exercise2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    class Exercise2
+    {
+        //public static int Add(int num1, int num2)
+        //{ 
+        //    return num1 + num2;
+        //}
+
+        public static int Add(params int[] numbers) //Params keyword allows any number of integers to be passed into an array called numbers.
+        {
+            int sum = 0; //Makes variable sum and sets to 0. 0 is the identity element for addition, ie, always set sum to 0 initially; Will add sum of integers passed in;
+            foreach (int num in numbers) //Declares integer variable named num to represent elements in numbers array in line 16.
+            {
+                sum += num; //Adds value of num to sum variable during each loop
+            }
+            return sum;        
+        }
+
+        //}
+        //public static int Multiply(int num1, int num2)
+        //{
+        //    return num1 * num2;
+
+
+        public static int Multiply(params int[] numbers) //Params keyword allows any number of integers to be passed into an array called numbers.
+        {
+            int product = 1; //Makes variable product and sets to 1. 1 is the identity element for multiplication, ie, always set product to 1 initially; Will multiply integers passed in.
+            foreach (int num in numbers) //Declares integer variable num to represent elements in numbers array from line 34.
+            {
+                product *= num; //Multiplies the value of num with the current value of product during each iteration of the loop.
+            }
+            return product;
+        }
+        
+        //public static int Subtract(int num1, int num2) 
+        //{
+        //    return num1 - num2;
+        //}
+
+        public static int Subtract(params int[] numbers) //Params keyword allows any number of integers to be passed into an array called numbers.
+        {
+            int result = numbers[0]; //Initializes result variable with the first item in the numbers array. Subtraction always starts with the first number in the array [0].
+            for (int i = 1; i < numbers.Length; i++) //starts I at the second item in the numbers array [1] and will continue until I is less than the length of numbers array.
+            {
+                result -= numbers[i]; //Subtracts values of numbers[i] from current value of result for each iteration; Subtracts all numbers in the numbers array from the initial value [0].
+            }
+            return result;
+        }
+
+        //public static int Divide(int num1, int num2)
+        //{
+        //    return num1 / num2;
+
+        public static int Divide(params int[] numbers) //Params keyword allows any number of integers to be passed into an array called numbers.
+        {
+            int quotient = numbers[0]; //Sets variable quotient to the zero index of the numbers array. Just like subtraction, division starts with the first number [0].
+            for(int i = 1;i < numbers.Length; i++) //Starts I at the second number in the array [1] and will continue until I is less than the length of the array.
+            {
+                quotient /= numbers[i]; //Divides the current value of quotient with the current value of numbers[i] during each iteration of the loop. Divides all numbers in the array by the initial value [0].
+            }
+            return quotient;
+        }
+    }
+}
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I want to be, the very _,");
+            var line1 = Console.ReadLine();
+
+            Console.WriteLine("That no one ever _.");
+            var line2 = Console.ReadLine();
+
+            Console.WriteLine("To catch them is my real _,");
+            var line3 = Console.ReadLine();
+
+            Console.WriteLine("To train them is my _.");
+            var line4 = Console.ReadLine();
+
+            Console.WriteLine($"I want to be, the very {line1}, that no one ever {line2}. \n" +
+            $"To catch them is my real {line3}, to train them is my {line4}.");
+
+            //Console.WriteLine(Exercise2.Add(2,4));
+            Console.WriteLine(Exercise2.Add(2,4,1,8,9));
+
+            //Console.WriteLine(Exercise2.Multiply(4,6));
+            Console.WriteLine(Exercise2.Multiply(1,2,3));
+
+            //Console.WriteLine(Exercise2.Subtract(8,4));
+            Console.WriteLine(Exercise2.Subtract(10,5,3,1));
+
+            //Console.WriteLine(Exercise2.Divide(10,2));
+            Console.WriteLine(Exercise2.Divide(15,3,5));
         }
     }
 }
